3b6064b5e6d83cb2666a3c47262a6b69e9330293
russ <russellarthursmith@yahoo.com>
2018-01-14 13:30:54 +0800

    setup basic scene and cam rotation

---
+++ Assets/Scripts/CamRotator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRotator : MonoBehaviour {
+    
+    float SmoothStep(float t) {
+        return t * t * (3 - 2 * t);
+    }
+
+    public void Rotate(bool positiveRot) {
+        float start = transform.eulerAngles.y;
+        float end = positiveRot ? start + 90 : start - 90;
+        StartCoroutine(DoRotate(start, end));
+    }
+
+    IEnumerator DoRotate(float start, float end) {
+        float t = 0;
+        while (t < 1) {
+            float curr = Mathf.Lerp(start, end, SmoothStep(t));
+            transform.eulerAngles = new Vector3(35.264f, curr, 0);
+            t += 5 * Time.deltaTime;
+            yield return null;
+        }
+        transform.eulerAngles = new Vector3(35.264f, end, 0);
+    }
+}
