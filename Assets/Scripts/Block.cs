39734a1e396969996aa8fba44d548140c79900a8
russ <russellarthursmith@yahoo.com>
2018-01-14 14:22:31 +0800

    block spawning working

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Block : MonoBehaviour {
 
+    public int value;
 	// Use this for initialization
 	void Start () {
 		
---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+
+    struct Space {
+        public Vector3 pos;
+        public Block block;
+    }
+
+    Space[] spaces;
+    Queue<Block> blockPool;
+    public GameObject blockPrefab;
+    int[] freeSpaces;
+
+    void Awake() {
+        spaces = new Space[27];
+        freeSpaces = new int[27]; 
+        blockPool = new Queue<Block>();
+
+        for(int i = 0; i < 27; i++) {
+            spaces[i].pos = new Vector3(i % 3 - 1, (i % 9) / 3 - 1, i / 9 - 1);
+            GameObject blockObj = Instantiate(blockPrefab);
+            blockObj.transform.position = new Vector3(1000, 1000, 1000);
+            blockPool.Enqueue(blockObj.GetComponent<Block>());
+        }
+    }
+
+    public void AddBlock() {
+        int count = 0;
+        for (int i = 0; i < 27; i++) {
+            if (spaces[i].block == null) {
+                freeSpaces[count] = i;
+                count++;
+            }
+        }
+        int num = freeSpaces[Random.Range(0, count)];
+        Block b = blockPool.Dequeue();
+        b.value = Random.Range(0, 10) < 9 ? 1 : 2;
+        b.transform.position = spaces[num].pos;
+        spaces[num].block = b;
+    }
+}
