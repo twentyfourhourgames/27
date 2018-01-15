216d1afa0f833f97ef559f5d103e0f9647eb59e5
russ <russellarthursmith@yahoo.com>
2018-01-15 11:17:04 +0800

    Simpler force portrait on desktop

---
+++ Assets/Scripts/ResManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResManager : MonoBehaviour {
+
+#if UNITY_STANDALONE
+    void Awake () {
+		int h = Screen.currentResolution.height - 100;
+        int w = h * 9 / 16;
+        Screen.SetResolution(w, h, false);
+	}
+#endif
+}
